459a5473716d907e174a1b1dc75605a9b00a9bf0
Sally Gitonga <gitongasally4@gmail.com>
2024-05-14 19:52:41 +0300

    complete assignment

--- passwordValidation.js
+++ passwordValidation.js
@@ -8,10 +8,19 @@ It cannot contain repeated characters in a row (like "1111" or "aaaaa" or "Jjjj"
 Optionally, you can add a list of common words that users shouldn't use as passwords (like "password" or "123456").*/
 
 
-let password=
+let password = "4rdhjbge7"
 
 function passwordValidation(password) {
-    if (password = null) {
-        
+    if (password.length === 0) {
+        console.log("Invalid Password. password is blank")
+    } else if (password.length < 8) {
+        console.log("Invalid Password. password too short")
     }
+    else if (password == "password" || password == "12345678") {
+        console.log("Invalid Password. password too easy")
+    } else {
+        console.log("Password is valid")
+    }
+    return password
 }
+console.log(passwordValidation(password))
